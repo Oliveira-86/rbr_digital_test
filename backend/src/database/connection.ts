28a16f644888f0252ba39fdfd32306a019d13b6f
oliveira-86-c1risk <egberto.duarte.14@gmail.com>
2024-06-07 19:52:35 -0300

    .

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
 
-// not woring ???
 const db_user = process.env.DB_USER;
 const db_password = process.env.DB_PASSWORD;
 
 export const connect = () => {
-    mongoose.connect(`mongodb+srv://tally:2y7rHrii3xl6dpeb@cluster0.rt6qf.mongodb.net/`)
+    mongoose.connect(`mongodb+srv://${db_user}:${db_password}@cluster0.rt6qf.mongodb.net/`)
 
     const connection = mongoose.connection;
 
